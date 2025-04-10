a6dcb36a264f0929033c4200527010ff8e7b9e6a
mirra12 <72499402+mirra12@users.noreply.github.com>
2025-04-10 12:35:10 +0200

    Update Allpeaks_PCA.R

--- Allpeaks_PCA.R
+++ Allpeaks_PCA.R
@@ -1,8 +1,11 @@
+library('tidyverse')
+library('ggplot2')
+library('pcaMethods')
+library('ggrepel')
 
+allPeaks <- read.csv("H2A.Z Peaks mouse Merged all samples.txt", sep = "\t")
 
-GeneClustering2 <- read.csv("H2A.Z Peaks mouse Merged all samples.txt", sep = "\t")
-
-df <- GeneClustering2 %>% select("X6.p7.Az.250.16_S14_R1_001.quantile.normalised", 
+df <- allPeaks %>% select("X6.p7.Az.250.16_S14_R1_001.quantile.normalised", 
                           "X4.p10B.AZ.250.14_S10_R1_001.quantile.normalised", 
                           "Pooled.1.p12.AZ.200_S1_L001_R1_001.quantile.normalised", 
                           "Pooled.3.NSN.AZ.200_S3_L001_R1_001.quantile.normalised", 
@@ -13,9 +16,6 @@ df <- GeneClustering2 %>% select("X6.p7.Az.250.16_S14_R1_001.quantile.normalised
                           "X13.bla.AZ460_S13_L001_R1_001.quantile.normalised")
 
 
-names(GeneClustering2)
-
-
 df <- setNames(df, c("P7", "P10", "P12", "NSN", "SN", "MII", "8cell", "Morula", "Blastocyst"))
 numerical_data <- apply(df, 2, function(x) as.numeric(gsub(",", ".", x)))
 
@@ -24,7 +24,7 @@ pcoutput <- pca(t(numerical_data))
 dat <- as.data.frame(scores(pcoutput)[,1:2])
 dat$celltype <- rownames(dat)
 
-
+#percentages taken from results above
 p <- ggplot(dat, aes(PC1, PC2, colour=celltype, label=celltype)) + 
   geom_point(size=3) + 
   geom_text_repel() + 
